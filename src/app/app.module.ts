1bf78c18bf2721a55fd0dff6cc495ea0a18714d2
ranuthd <ranuthd@codegen.net>
2024-08-18 23:48:42 +0530

    Started working on the header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,16 @@ import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatBadgeModule } from '@angular/material/badge';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +36,7 @@ import { AppComponent } from './app.component';
     MatToolbarModule,
     MatTableModule,
     MatSnackBarModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
